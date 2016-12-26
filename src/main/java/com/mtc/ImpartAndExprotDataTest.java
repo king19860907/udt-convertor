076c9ec17193e5016e441fc52b5bcc2a04ba6ed7
jun_ma <ludy19860907@163.com>
2016-12-26 10:52:01 -0800

    test

--- src/main/java/com/mtc/ImpartAndExprotDataTest.java
+++ src/main/java/com/mtc/ImpartAndExprotDataTest.java
@@ -1,9 +1,11 @@
 package com.mtc;
 
 import com.mtc.dao.BaseDao;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -28,21 +30,26 @@ public class ImpartAndExprotDataTest {
     public static void main(String[] args) {
         //String tableName = "OCNT";
         String suffix = "SBODEMOUS";
-
+        List<String> excludeTables = Arrays.asList("ONNM","NNM1","NNM2","NNM3","NNM4","NNM5","NNM6");
+        
         ImpartAndExprotDataTest test = new ImpartAndExprotDataTest();
-        /*List<String> tableNames = test.getAllTalbeNames();
-        List<String> subTableNames = tableNames.subList(tableNames.indexOf("U_SURP2")+1,tableNames.size());
-        for(String talbeName : subTableNames){
-        	System.out.println("index:"+tableNames.indexOf(talbeName));
-            System.out.println("tableName:"+talbeName);
-            try{
-                test.copyDate(talbeName,suffix);
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-*/
-        test.copyDate("OUSR",suffix);
+       // List<String> tableNames = test.getAllTalbeNames();
+       // List<String> subTableNames = tableNames.subList(tableNames.indexOf("U_SURP2")+1,tableNames.size());
+       // for(String talbeName : subTableNames){
+       // 	if(!excludeTables.contains(talbeName)){
+       // 		System.out.println("index:"+tableNames.indexOf(talbeName));
+        //        System.out.println("tableName:"+talbeName);
+         //       try{
+          //          test.copyDate(talbeName,suffix);
+           //     }catch (Exception e){
+            //        System.out.println(e.getMessage());
+             //   }
+        //	}else{
+        //		System.out.println("排除的表："+talbeName);
+        //	}
+        //}
+
+        test.copyDate("@U_ITRN1",suffix);
     }
 
     public List<String> getAllTalbeNames(){
@@ -65,13 +72,13 @@ public class ImpartAndExprotDataTest {
             hanaTableName = tableName;
         }
         final String hanaTableName2 = hanaTableName;
-        baseDaoByHana.deleteResult(suffix, hanaTableName2);
+        //baseDaoByHana.deleteResult(suffix, hanaTableName2);
         System.out.println(list.size());
         list.stream().map(column->{
             try{
                 baseDaoByHana.insertResult(suffix, hanaTableName2, column);
             }catch(Exception e){
-                e.printStackTrace();
+                System.out.println("error:"+e.getMessage());
             }
             return null;
         }).collect(Collectors.toList());
